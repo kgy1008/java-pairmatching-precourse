57a16b3d654ec90750addeccb691be8f9ed38621
kgy1008 <gayeon1008@g.skku.edu>
2024-12-10 18:36:33 +0900

    feat: 컨트롤러 객체 생성

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,12 @@
 package pairmatching;
 
+import pairmatching.config.AppConfig;
+import pairmatching.controller.PairController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        AppConfig appConfig = new AppConfig();
+        PairController controller = appConfig.controller();
+        controller.run();
     }
 }
