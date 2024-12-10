91ead96ed835a42b5e83000c8d1f7d6c76ce31d1
kgy1008 <gayeon1008@g.skku.edu>
2024-12-10 19:26:15 +0900

    feat: 미션 객체 생성

---
+++ src/main/java/pairmatching/domain/curriculum/Mission.java
@@ -0,0 +1,12 @@
+package pairmatching.domain.curriculum;
+
+public class Mission {
+
+    private final String name;
+    private final Level level;
+
+    public Mission(final String name, final Level level) {
+        this.name = name;
+        this.level = level;
+    }
+}
